1d3c844f1ec0ed3540a5466973868db117384601
Ethan Jimenez <erosethanjv@gmail.com>
2014-03-20 11:49:19 -0600

    Updated number automata, created identifier automata

--- token.py
+++ token.py
@@ -1,68 +1,76 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 def esDigito(char):
 	return '0' <= char <= '9'
 
-def tokenEntero(expresion):
-	indice = 0
-	estado = 'E0'
-	
-	while estado != 'E2':
-		sigEstado = 'E2'
-		char = expresion[indice]
-		
-		if estado == 'E0':
-			if esDigito(char):
-				sigEstado = 'E1'
-		
-		elif estado == 'E1':
-			if esDigito(char):
-				sigEstado = 'E1'
-			else:
-				return indice
-		
-		estado = sigEstado
-		indice += 1
-	
-	return 0
-	
-def tokenFlotante(expresion):
-	indice = 0
-	estado = 'F0'
-	
-	while estado != 'F4':
-		sigEstado = 'F4'
-		char = expresion[indice]
-		
-		if estado == 'F0':
-			if esDigito(char):
-				sigEstado = 'F1'
-			elif char == '.':
-				sigEstado = 'F2'
-		
-		elif estado == 'F1':
-			if esDigito(char):
-				sigEstado = 'F1'
-			elif char == '.':
-				sigEstado = 'F3'
-			else:
-				return indice
-		
-		elif estado == 'F2':
-			if esDigito(char):
-				sigEstado = 'F3'
-		
-		elif estado == 'F3':
-			if esDigito(char):
-				sigEstado = 'F3'
-			else:
-				return indice
-		
-		estado = sigEstado
-		indice += 1
-	
-	return 0
+def esIdValido(char):
+	if 'a' <= char <= 'z':
+		return True
+	if 'A' <= char <= 'Z':
+		return True
+	return char == '_'
 
-test = raw_input() + '\n'
-print tokenFlotante(test)
-print tokenEntero(test)
+def tokenIdentificador(expresion):
+	indice = 0
+	estado = 'I0'
+	
+	while estado != 'I2':
+		sigEstado = 'I2'
+		char = expresion[indice]
+		
+		if estado == 'I0':
+			if esIdValido(char):
+				sigEstado = 'I1'
+		
+		if estado == 'I1':
+			if esIdValido(char):
+				sigEstado = 'I1'
+			elif esDigito(char):
+				sigEstado = 'I1'
+			else:
+				return ('I', indice)
+		
+		estado = sigEstado
+		indice += 1
+	
+	return ('N', 0)
+
+def tokenNumero(expresion):
+	indice = 0
+	estado = 'N0'
+	
+	while estado != 'N4':
+		sigEstado = 'N4'
+		char = expresion[indice]
+		
+		if estado == 'N0':
+			if esDigito(char):
+				sigEstado = 'N1'
+			elif char == '.':
+				sigEstado = 'N2'
+		
+		elif estado == 'N1':
+			if esDigito(char):
+				sigEstado = 'N1'
+			elif char == '.':
+				sigEstado = 'N3'
+			else:
+				return ('E', indice)
+		
+		elif estado == 'N2':
+			if esDigito(char):
+				sigEstado = 'N3'
+		
+		elif estado == 'N3':
+			if esDigito(char):
+				sigEstado = 'N3'
+			else:
+				return ('F', indice)
+		
+		estado = sigEstado
+		indice += 1
+	
+	return ('N', 0)
+
+test = input() + '\n'
+print(tokenIdentificador(test))
