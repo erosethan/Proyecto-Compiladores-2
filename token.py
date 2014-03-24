e3f966670c98fe2d74dfa4d13c25733a24da4751
Alberto Medina <blackdrako013@hotmail.com>
2014-03-24 11:58:05 -0600

    First version finished

--- token.py
+++ token.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import sys
 
 def esDigito(char):
@@ -162,7 +163,7 @@ def tokenSimboloEsp(expresion):
 		'(': ('(', 1),
 		')': (')', 1),
 		'{': ('{', 1),
-		'}': ('}',1),
+		'}': ('}', 1),
 		'[': ('[', 1),
 		']': (']', 1),
 		';': (';', 1),
@@ -186,36 +187,150 @@ def reconocerToken(expresion):
 	
 	return tokenSimboloEsp(expresion)
 
-def estaEnAlfabeto(caracter):
+def enAlfabeto(caracter):
 	ascii = ord(caracter)
-	return ( ( ascii >= 0 and ascii <= 35) or 
-				( ascii >= 37 and ascii <= 62 ) or
-				( ascii >= 65 and ascii <= 91 ) or
-				( ascii >= 97 and ascii <= 125 ) or
-				ascii == 93 )
+	return (
+		(ascii >= 0 and ascii <= 35) or 
+		(ascii >= 37 and ascii <= 62) or
+		(ascii >= 65 and ascii <= 91) or
+		(ascii >= 97 and ascii <= 125) or
+		ascii == 93 or ascii == 95
+	)
+				
+def valor(cadena):
+	return {
+		'(': 'PARENTESIS-ABIERTO',
+		')': 'PARENTESIS-CERRADO',
+		'{': 'LLAVE-ABIERTA',
+		'}': 'LLAVE-CERRADA',
+		'[': 'CORCHETE-ABIERTO',
+		']': 'CORCHETE-CERRADO',
+		';': 'PUNTO-Y-COMA',
+		',': 'COMA',
+		'.': 'PUNTO',
+		':': 'DOS-PUNTOS',
+		'#': 'NUMERAL',
+		'\'': 'COMILLA-SIMPLE',
+		'"': 'COMILLA-DOBLE',
+		'++': 'INCREMENTO-POSITIVO',
+		'--': 'INCREMENTO-NEGATIVO',
+		'=': 'ASIGNACION',
+		'+=': 'SUMA-ASIGNACION',
+		'-=': 'RESTA-ASIGNACION',
+		'*=': 'MULTIPLICACION-ASIGNACION',
+		'/=': 'DIVISION-ASIGNACION',
+		'if': 'IF',
+		'else': 'ELSE',
+		'switch': 'SWITCH',
+		'case': 'CASE',
+		'for': 'FOR',
+		'while': 'WHILE',
+		'do': 'DO',
+		'&&': 'Y',
+		'||': 'O',
+		'!': 'NEGACION',
+		'!=': 'DIFERENTE',
+		'>': 'MAYOR',
+		'<': 'MENOR',
+		'==': 'IGUAL',
+		'>=': 'MAYOR-IGUAL',
+		'<=': 'MENOR-IGUAL',
+		'+': 'SUMA',
+		'-': 'RESTA',
+		'*': 'MULTIPLICACION',
+		'/': 'DIVISION',
+		'%': 'MODULO',
+		'int': 'INT',
+		'float': 'FLOAT',
+		'double': 'DOUBLE',
+		'char': 'CHAR',
+		'void': 'VOID',
+		'short': 'SHORT',
+		'long': 'LONG',
+		'signed': 'SIGNED',
+		'unsigned': 'UNSIGNED',
+		'include': 'INCLUDE',
+		'define': 'DEFINE',
+		'inline': 'INLINE',
+		'return': 'RETURN',
+		'main': 'MAIN'
+	}.get(cadena, cadena)
+	
+def componenteLexico(token, cadena):
+	selectivas = set(['if', 'else', 'switch', 'case'])
+	repetitivas = set(['for', 'while', 'do'])
+	tiposDatos = set(['void', 'int', 'float', 'double', 'char'])
+	calificadoresTipoDato = set(['short', 'long', 'signed', 'unsigned'])
+	preprocesador = set(['include', 'define', 'inline'])
+	Ireturn = set(['return'])
+	Imain = set(['main'])
+	logico = set(['&&', '||', '!', '!='])
+	relacional = set(['>', '<', '==', '>=', '<='])
+	aritmetico = set(['+', '-', '*', '/', '%'])
+	asignacion = set(['=', '+=', '-=', '*=', '/='])
+	incremento = set(['++', '--'])
+	
+	if cadena in selectivas:
+		return 'ESTRUCTURA SELECTIVA'
+	if cadena in repetitivas:
+		return 'ESTRUCTURA REPETITIVA'
+	if cadena in tiposDatos:
+		return 'TIPO DE DATO'
+	if cadena in calificadoresTipoDato:
+		return 'CALIFICADOR DE TIPO DE DATO'
+	if cadena in preprocesador:
+		return 'INSTRUCCION DE PREPROCESADOR'
+	if cadena in Ireturn:
+		return 'RETURN'
+	if cadena in Imain:
+		return 'MAIN'
+	if cadena in logico:
+		return 'OPERADOR LOGICO'
+	if cadena in relacional:
+		return 'OPERADOR RELACIONAL'
+	if cadena in aritmetico:
+		return 'OPERADOR ARITMETICO'
+	if cadena in asignacion:
+		return 'ASIGNACION'
+	if cadena in incremento:
+		return 'INCREMENTO'
+	if token == 'E':
+		return 'NUMERO ENTERO'
+	if token == 'F':
+		return 'NUMERO FLOTANTE'
+	if token == 'I':
+		return 'IDENTIFICADOR'
+	return 'SIMBOLO ESPECIAL'
+	
 	
 def main(nEntrada, nSimbolos, nErrores):
 	arcEntrada = open( nEntrada, 'r' )
 	arcSimbolos = open( nSimbolos, 'w' )
 	arcErrores = open( nErrores, 'w' )
-	arcErrores.write('Linea de Error\tSimbolo\n')
+
+	arcSimbolos.write('%30s %16s %22s\n' % ('Componente Lexico', 'Lexema', 'Valor'))
+	arcErrores.write('%15s %10s\n' % ('Linea de Error', 'Simbolo'))
+	
 	texto = arcEntrada.read()
 	numLinea = 1
 	while len(texto) > 0:
 		token = reconocerToken(texto)
-		#Hay que checar si esta en el alfabeto
 		if texto[0] == '\n':
 			print(numLinea)
 			numLinea = numLinea + 1
 		car = texto[0]
+		lexema = texto[0: token[1]]
+		if(token[0] != 'N'):
+			arcSimbolos.write('%30s %16s %22s\n' % (componenteLexico(token[0], lexema), lexema, valor(lexema)))
 		texto = texto[token[1]: len(texto)]
 		print(car)
-		if( not(estaEnAlfabeto(car)) ):
-			arcErrores.write( car + '\t\t\t\t' + str(numLinea) + '\n' )
+		if( not(enAlfabeto(car)) ):
+			arcErrores.write('%15s %10s\n' % (str(numLinea), car))
 		print(token)
 		if token[0] != 'N':
 			print(token)
 	arcErrores.close()
+	arcSimbolos.close()
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2], sys.argv[3])
